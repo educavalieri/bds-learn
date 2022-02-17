c8da2518a20fdf3ee6b20362345acb1efbcffa18
educavalieri <eduardocavalieri@hotmail.com>
2022-02-17 11:40:35 -0300

    insert Exceptions, validations and Security

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
@@ -1,7 +1,8 @@
 package com.devsuperior.dslearnbds.repositories;
 
 import com.devsuperior.dslearnbds.entities.Course;
+import com.devsuperior.dslearnbds.entities.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResourceRepository extends JpaRepository<ResourceRepository, Long> {
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
 }
