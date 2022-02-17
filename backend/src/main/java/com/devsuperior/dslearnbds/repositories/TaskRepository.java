b6a5234e455ffe1d8dbdca2b0b23dbafb1fc4484
educavalieri <eduardocavalieri@hotmail.com>
2022-02-17 10:22:32 -0300

    insert repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import com.devsuperior.dslearnbds.entities.Course;
+import com.devsuperior.dslearnbds.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
