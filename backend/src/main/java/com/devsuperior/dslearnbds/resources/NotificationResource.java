16443c6d2d1294800e417eb9c3e2ef56859fc63b
educavalieri <eduardocavalieri@hotmail.com>
2022-02-23 16:40:21 -0300

    insert Query on Notifications Repository, Service and Resource

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +19,10 @@ public class NotificationResource {
     private NotificationService notificationService;
 
     @GetMapping
-    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(Pageable pageable){
-        Page<NotificationDTO> page = notificationService.notificationsForCurrentUser(pageable);
+    public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(
+            @RequestParam(value = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
+            Pageable pageable){
+        Page<NotificationDTO> page = notificationService.notificationsForCurrentUser(pageable, unreadOnly);
         return ResponseEntity.ok().body(page);
     }
 
